bd7b11189915b98d8915fc508d493208840b15c0
Alexander Shorin <kxepal@gmail.com>
2018-06-30 15:08:47 +0300

    Add example with middlewares
    It's broken in expected way, fix comes in following commit since
    it has different authorship.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -33,3 +33,23 @@ def test_hello(aiohttp_client, loop):
     data = yield from resp.json()
     assert data == {'text': 'Hello World!'}
 
+
+@asyncio.coroutine
+def test_middleware(aiohttp_client, loop):
+    mod = load_example('2-middleware')
+    app = mod.create_app(loop)
+    client = yield from aiohttp_client(app)
+
+    # Hello
+    resp = yield from client.get('/')
+    assert resp.status == 200
+
+    data = yield from resp.json()
+    assert data == {'counter': 1}
+
+    # 404
+    resp = yield from client.get('/thing')
+    assert resp.status == 404
+
+    data = yield from resp.json()
+    assert data == {'error': 'not_found', 'reason': 'Not Found'}
