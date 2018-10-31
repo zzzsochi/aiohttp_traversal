50477b4f2810dd1e52f2b6f9b67d0bf2e6508b25
Alexander Zelenyak <zzz.sochi@gmail.com>
2018-10-31 13:51:51 +0300

    Bump version: 0.10.0 → 0.11.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='aiohttp_traversal',
-    version='0.10.0',
+    version='0.11.0',
     description='Traversal based router for aiohttp.web',
     classifiers=[
         "License :: OSI Approved :: BSD License",
